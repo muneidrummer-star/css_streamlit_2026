caad72d2cfe0f0c87d045894b294359f6445a542
muneidrummer-star <muneidrummer@gmail.com>
2026-01-31 16:39:44 +0200

    Update app_profiler_menus.py

--- app_profiler_menus.py
+++ app_profiler_menus.py
@@ -42,10 +42,10 @@ def load_cv():
     with open("Munei_Mugeri_CV.pdf", "rb") as file:
         return file.read()
 
-def get_current_weather(city = "Pretoria", api_key = "ed62b3f8bd037b703286ac1ac37e39e8"):
+def get_current_weather(city = "Pretoria", ed62b3f8bd037b703286ac1ac37e39e8"):
     url = (
         f"https://api.openweathermap.org/data/2.5/weather"
-        f"?q={city}&appid={api_key}&units=metric"
+        f"?q={Pretoria}&appid={ed62b3f8bd037b703286ac1ac37e39e8}&units=metric"
     )
     response = requests.get(url)
     if response.status_code == 200:
@@ -59,10 +59,10 @@ def get_current_weather(city = "Pretoria", api_key = "ed62b3f8bd037b703286ac1ac3
         }
     return None
 
-def get_weather_forecast(city = "Pretoria", api_key = "ed62b3f8bd037b703286ac1ac37e39e8"):
+def get_weather_forecast(Pretoria, api_key = "ed62b3f8bd037b703286ac1ac37e39e8"):
     url = (
         f"https://api.openweathermap.org/data/2.5/forecast"
-        f"?q={city}&appid={api_key}&units=metric"
+        f"?q={Pretoria}&appid={ed62b3f8bd037b703286ac1ac37e39e8}&units=metric"
     )
     response = requests.get(url)
     if response.status_code == 200:
@@ -220,4 +220,5 @@ elif menu == "Contact":
 
     st.info("📧 Email: muneidrummer@gmail.com")
     st.success("🔗 LinkedIn: https://www.linkedin.com")
+
 
